818d89540260ccf5ead2426a2c53be26a62bb9b9
jessica piper <jpiper231@gmail.com>
2020-07-05 20:35:04 -0400

    fixed pie legends

--- src/_scripts/_charts.js
+++ src/_scripts/_charts.js
@@ -109,7 +109,7 @@ var margin = {top: 20, right:20, bottom:120, left:140};
 
 var container = d3.select(element);
 var containerWidth = 600//container.node().offsetWidth;
-var containerHeight = 300;
+var containerHeight = 270;
 var chartWidth = containerWidth - margin.right - margin.left;
 var chartHeight = containerHeight - margin.top - margin.bottom;
 
@@ -187,25 +187,25 @@ var legend = svg.append("g")
   .attr("width",series.length * 36)
   .attr("height",40)
   .attr("font-family", "helvetica")
-  .attr("font-size", 14)
+  .attr("font-size", 10)
   //.style("margin-left", `${margin.left}px`)
   .attr("text-anchor", "left")
   .style("display", "block")
   .selectAll("g")
   .data(series)//(keys)
   .join("g")
-    .attr("transform", (d, i) => `translate(${i * 100},0)`);
+    .attr("transform", (d, i) => `translate(${i * 60},0)`);
 
 legend.append("rect")
       .attr("x", 0)
       .attr("y",0)
-      .attr("width", 36)
-      .attr("height", 25)
+      .attr("width", 20)
+      .attr("height", 15)
       .attr("fill", d => color(d.key));
 
 legend.append("text")
   .attr("x", 0)
-  .attr("y",32)
+  .attr("y",25)
   .attr("dy", "0.35em")
   .text(d => d.key);
 
@@ -213,10 +213,9 @@ legend.append("text")
 
 thirdRound('#rcv3');
 
-function secondRound(element){
+function secondRound(element,lastname){
 
-var margin = {top: 10, right:20, bottom:20, left:5};
-
+var margin = {top: 0, right:0, bottom:0, left:0};
 
 var container = d3.select(element);
 var containerWidth = 300//container.node().offsetWidth;
@@ -230,9 +229,10 @@ var svg = container.append('svg')
             .append('g')
             .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-var radius = containerWidth/2 - 80;
+var radius = containerWidth/2 - 60;
 
-var data = round2.map(d => d.Brakey)//{a: 9, b: 20, c:30, d:8, e:12}
+var data = round2.map(d => d[lastname])//{a: 9, b: 20, c:30, d:8, e:12}
+var labels = round2.map(d => d.candidate)
 
 //tooltip
 var tooltip = svg.append('text')
@@ -240,46 +240,79 @@ var tooltip = svg.append('text')
 
 // set the color scale
 var color = d3.scaleOrdinal()
-  .domain(data)
-  .range(["orange", "blue", "yellow", "green", "red"])
+  .domain(data[1,2,3])
+  .range(["#8c1a0f", "#eb2815", "#ffea00", "#9ae3cc"])
 
 // Compute the position of each group on the pie:
 var pie = d3.pie()
   .value(function(d) {return d.value; })
 var data_ready = pie(d3.entries(data))
 
+//var data_sub = subset(data_ready,0,3)
 // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
 svg.selectAll('g')
   .data(data_ready)
   .enter()
   .append('path')
   .attr('d', d3.arc()
-    .innerRadius(0)
+    .innerRadius(1)
     .outerRadius(radius)
   )
   .attr('fill', function(d){ return(color(d.data.key)) })
   .attr("stroke", "white")
   .style("stroke-width", "1px")
-  .style("opacity", 0.7)
+  .style("opacity", 1)
   .attr("transform", `translate(${chartWidth/2},${chartHeight/2})`)
   .on('mouseenter', function(d) {
-      //var coordinates= d3.mouse(this);
-      //var x = 50 //d.data.key//var xPosition = coordinates[0]// + 25//xScale(d[1])+15//400//xScale(d.ballot1) + 0.1//coordinates[0]+10//coordinates[0];
-      //var y = 50 //d.data.key //var yPosition = coordinates[1]// + 25//chartHeight + 20//yScale(d.data.candidate)+28//coordinates[1]-10//coordinates[1] + 25;
-      d3.select(this).classed('highlight-red', true);
-      tooltip.html(d.data.value*100 + "%")//(d3.format("$,.0f")(xScale(d[1]-d[0]))
+      d3.select(this).classed('highlight-gray', true);
+      tooltip.html(labels[d.data.key] + ": " + d.data.value*100 + "%")//(d3.format("$,.0f")(xScale(d[1]-d[0]))
             .style("opacity", 1)
-            .attr('transform',`translate(50, 50)`)//(${xPosition}, ${yPosition}) `)
+            .attr('transform',`translate(200, 50)`)//(${xPosition}, ${yPosition}) `)
             //.attr('transform',`translate(arcGenerator.centroid(d))`)
     })
     .on('mouseleave', function(d) {
-      d3.select(this).classed('highlight-red', false);
+      d3.select(this).classed('highlight-gray', false);
       tooltip.text('');
     });
 
+var legend = svg.append("g")
+  .attr("width",data_ready.length * 36)
+  .attr("height",40)
+  .attr("font-family", "helvetica")
+  .attr("font-size", 10)
+  //.style("margin-left", `${margin.left}px`)
+  .attr("text-anchor", "left")
+  .style("display", "block")
+  .selectAll("g")
+  .data(function(d){
+    if(data[0]==0){
+      return(data_ready.slice(1,4));
+    }else if (data[2]==0) {
+      return(data_ready.slice(0,2).concat(data_ready.slice(3,4)));
+    }else{
+      return(data_ready.slice(0,1).concat(data_ready.slice(2,4)));
+    }
+  })
+  .join("g")
+    .attr("transform", (d, i) => `translate(${i * 90},0)`);
+
+legend.append("rect")
+      .attr("x", 5)
+      .attr("y",0)
+      .attr("width", 20)
+      .attr("height", 15)
+      .attr("fill", d => color(d.data.key));
+
+legend.append("text")
+  .attr("x", 5)
+  .attr("y",25)
+  .attr("dy", "0.35em")
+  .text(d => labels[d.data.key]);
+
 }
 
-secondRound('#rcv2');
-
+secondRound('#BrakeyVoters','Brakey');
+secondRound('#BennettVoters','Bennett');
+secondRound('#CraftsVoters','Crafts');
 
 console.log('hello, this is my charts file!');
